c9e635350a117512394323bd08c84d83f4cc5cd1
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-15 17:51:37 -0600

    working on fixing it all

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,3 @@
-let routes = require('./db.json')
-
-let globalID = 4;
-
 module.exports = {
   getCompliment: (req, res) => {
       const compliments = ["You floated that route!",
@@ -28,37 +24,5 @@ module.exports = {
     let randomPrediction = predictions[randomIndex];
 
     res.status(200).send(randomPrediction);     
-  },
-  getRoutes: (req, res) => {
-    res.status(200).send(routes)
-  },
-  deleteRoute: (req, res) => {
-    const {id} = req.params;
-        let index = routes.findIndex(elem => elem.id === +req.params.id);
-        routes.splice(index, 1);
-        res.status(200).send(routes)
-  },
-  addRoute: (req, res) => {
-    const {name, difficulty, grade, ticks, imageURL} = req.body;
-    let newRoute = {
-        id: globalID,
-        name,
-        difficulty,
-        grade,
-        ticks,
-        imageURL
-    };
-    routes.push(newRoute);
-    globalID++;
-    res.status(200).send(routes);
-  },
-  addTick: (req, res) => {
-    let {id} = req.params;
-    let {type} = req.body;
-    let index = routes.findIndex(elem => elem.id === +id);
-    if (type === 'plus') {
-        routes[index].ticks += 1;
-        res.status(200).send(routes);
-    } else res.status(400).send('Something went wrong')
   }
 }
--- server/index.js
+++ server/index.js
@@ -6,13 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // When we want to be able to accept JSON.
 
-const {getCompliment, getPrediction, getRoutes, deleteRoute, addRoute, addTick} = require('./controller');
+const {getCompliment, getPrediction} = require('./controller');
 
 app.get('/api/compliment', getCompliment);
 app.get('/api/prediction', getPrediction);
-app.get('/api/routes', getRoutes);
-app.delete('.api/routes/:id', deleteRoute);
-app.put('/api/routes', addTick);
-app.post('/api/routes', addRoute);
 
 app.listen(4000, () => console.log("Server running on 4000"));
