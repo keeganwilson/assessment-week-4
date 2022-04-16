50dc13a378040ec166b5ad6d0275b36256f6d4eb
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-16 16:06:48 -0600

    can't figure it out!

--- client/main.js
+++ client/main.js
@@ -19,43 +19,43 @@ document.getElementById("predictButton").addEventListener('click', () => {
         });
 });
 
-const routesCallback = ({data: routes}) => displayRoutes(routes);
-const errCallback = err => console.log(err);
+const routesCallback = ({ data: routes }) => displayRoutes(routes)
+const errCallback = err => console.log(err)
 
 const getAllRoutes = () => axios.get(`${baseURL}/routes`).then(routesCallback).catch(errCallback);
-const addRoute = () => axios.get(`${baseURL}/routes`, body).then(routesCallback).catch(errCallback);
-const deleteRoute = () => axios.get(`${baseURL}/routes/${id}`).then(routesCallback).catch(errCallback);
-const addTick = () => axios.get(`${baseURL}/routes/${id}`, {type}).then(routesCallback).catch(errCallback);
+// const addRoute = () => axios.get(`${baseURL}/routes`, body).then(routesCallback).catch(errCallback);
+// const deleteRoute = () => axios.get(`${baseURL}/routes/${id}`).then(routesCallback).catch(errCallback);
+// const addTick = () => axios.get(`${baseURL}/routes/${id}`, {type}).then(routesCallback).catch(errCallback);
 
-const submitHandler = event => {
-  event.prevenDefault()
+// const submitHandler = event => {
+//   event.preventDefault()
 
-  let name = document.querySelectory('#name');
-  let diff = document.querySelectory('input[name="diff"]:checked');
-  let grade = document.querySelectory('input[name="grade"]:checked');
-  let ticks = document.querySelectory('input[name="ticks"]:checked');
-  let imageURL = document.querySelectory('#img');
+//   let name = document.querySelectory('#name');
+//   let diff = document.querySelectory('input[name="diff"]:checked');
+//   let grade = document.querySelectory('input[name="grade"]:checked');
+//   let ticks = document.querySelectory('input[name="ticks"]:checked');
+//   let imageURL = document.querySelectory('#route-image');
 
-  let bodyObj = {
-    name: name.value,
-    diff: diff.value,
-    grade: grade.value,
-    ticks: ticks.value,
-    imageURL: imageURL.value
-  }
+//   let bodyObj = {
+//     name: name.value,
+//     diff: diff.value,
+//     grade: grade.value,
+//     ticks: ticks.value,
+//     imageURL: imageURL.value
+//   }
 
-  createRoute(bodyObj);
+//   createRoute(bodyObj);
 
-  name.value = ''
-  diff.checked = false
-  grade.checked = false
-  ticks.checked = false
-  imageURL = ''
-}
+//   name.value = ''
+//   diff.checked = false
+//   grade.checked = false
+//   ticks.checked = false
+//   imageURL = ''
+// }
 
 const createRouteCard = route => {
   const routeCard = document.createElement('div');
-  createRouteCard.classList.add('route-card');
+  routeCard.classList.add('route-card');
 
   routeCard.innerHTML = `
   <img class='route-image' alt='route image' src=${route.imageURL}/>;
@@ -71,16 +71,17 @@ const createRouteCard = route => {
   routesContainer.appendChild(routeCard);
 }
 
-document.getElementById('addTickBtn').addEventListener('click', addTick());
-document.getElementById('deleteBtn'). addEventListener('click', deleteRoute());
+//document.getElementById('addTickBtn').addEventListener('click', addTick());
+//document.getElementById('deleteBtn'). addEventListener('click', deleteRoute());
 
 const displayRoutes = arr => {
-  routesContainer.innerHTML = ``
+  console.log(arr)
+  routesContainer.innerHTML = ''
   for(let i = 0; i < arr.length; i++) {
     createRouteCard(arr[i]);
   }
 }
 
-form.addEventListener('submit', submitHandler())
+//form.addEventListener('submit', submitHandler())
 
-getAllHouses();
+getAllRoutes();
--- server/routesCtrl.js
+++ server/routesCtrl.js
@@ -4,30 +4,29 @@ let globalID = 4;
 
 module.exports = {
     getAllRoutes: (req, res) => res.status(200).send(routes),
-
-    deleteRoute: (req, res) => {
-        let index = routes.findIndex(elem => elem.id === +req.params.id);
-        routes.splice(index, 1);
-        res.status(200).send(routes)
-  },
-    addRoute: (req, res) => {
-        const {name, difficulty, grade, ticks, imageURL} = req.body;
-        let newRoute = {
-            id: globalID,
-            name,
-            difficulty,
-            grade,
-            ticks,
-            imageURL
-        };
-        routes.push(newRoute);
-        globalID++;
-        res.status(200).send(routes);
-  },
-    addTick: (req, res) => {
-        let {id} = req.params;
-        let index = routes.findIndex(elem => elem.id === +id);
-        routes[index].ticks += 1;
-        res.status(200).send(routes);
-  }
+//     deleteRoute: (req, res) => {
+//         let index = routes.findIndex(elem => elem.id === +req.params.id);
+//         routes.splice(index, 1);
+//         res.status(200).send(routes)
+//   },
+//     addRoute: (req, res) => {
+//         const {name, difficulty, grade, ticks, imageURL} = req.body;
+//         let newRoute = {
+//             id: globalID,
+//             name,
+//             difficulty,
+//             grade,
+//             ticks,
+//             imageURL
+//         };
+//         routes.push(newRoute);
+//         globalID++;
+//         res.status(200).send(routes);
+//   },
+//     addTick: (req, res) => {
+//         let {id} = req.params;
+//         let index = routes.findIndex(elem => elem.id === +id);
+//         routes[index].ticks += 1;
+//         res.status(200).send(routes);
+//   }
 }
